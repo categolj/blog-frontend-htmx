99facdbdce8cbecf4dfbe205413bdef2d25d736e
Toshiaki Maki <makingx@gmail.com>
2026-04-26 15:42:14 +0900

    Update circuit breaker lifecycle to use logging implementation and bump version to 0.1.1

--- src/main/java/am/ik/blog/config/AppConfig.java
+++ src/main/java/am/ik/blog/config/AppConfig.java
@@ -4,19 +4,14 @@ import java.time.Duration;
 import java.time.InstantSource;
 
 import am.ik.spring.http.client.RetryableClientHttpRequestInterceptor;
-import am.ik.spring.http.client.circuitbreaker.CircuitBreaker;
 import am.ik.spring.http.client.circuitbreaker.CircuitBreakerClientHttpRequestInterceptor;
 import am.ik.spring.http.client.circuitbreaker.CircuitBreakerConfig;
-import am.ik.spring.http.client.circuitbreaker.CircuitBreakerLifecycle;
 import am.ik.spring.http.client.circuitbreaker.CircuitBreakerRegistry;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import am.ik.spring.http.client.circuitbreaker.LoggingCircuitBreakerLifecycle;
 import org.springframework.boot.restclient.RestClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportRuntimeHints;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.util.backoff.ExponentialBackOff;
 import org.zalando.logbook.Logbook;
 import org.zalando.logbook.spring.LogbookClientHttpRequestInterceptor;
@@ -41,25 +36,12 @@ public class AppConfig {
 			.waitDurationInOpenState(Duration.ofMinutes(1))
 			.permittedNumberOfCallsInHalfOpenState(5)
 			.build());
-		Logger logger = LoggerFactory.getLogger(AppConfig.class);
 		return restClientBuilder -> restClientBuilder
 			.requestInterceptor(new LogbookClientHttpRequestInterceptor(logbook))
 			.requestInterceptor(new RetryableClientHttpRequestInterceptor(backOff))
 			.requestInterceptor(CircuitBreakerClientHttpRequestInterceptor.builder()
 				.registry(circuitBreakerRegistry)
-				.lifecycle(new CircuitBreakerLifecycle() {
-					@Override
-					public void onFailure(CircuitBreaker circuitBreaker, HttpRequest request,
-							ResponseOrException responseOrException) {
-						logger.info("Circuit breaker request failed: {} {}", request.getMethod(), request.getURI());
-					}
-
-					@Override
-					public void onCallNotPermitted(CircuitBreaker circuitBreaker, HttpRequest request) {
-						logger.info("Circuit breaker request not permitted: {} {}", request.getMethod(),
-								request.getURI());
-					}
-				})
+				.lifecycle(LoggingCircuitBreakerLifecycle.builder().build())
 				.build());
 	}
 
